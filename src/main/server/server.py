51e69228fcb0d63f0cba35da45fbce286db6565a
programadorpython03 <programadorpython03@gmail.com>
2025-02-18 11:13:42 -0300

    feat: Implementing first route

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from src.main.server.routes.event import event_route_bp
 
 app = Flask(__name__)
+
+app.register_blueprint(event_route_bp)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000, debug=True)
 
